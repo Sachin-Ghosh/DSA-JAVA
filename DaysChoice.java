8b085661b7ceb6ce84396fc03a1eb5ee47447acd
SACHIN <ghoshsachin2301@gmail.com>
2024-06-28 13:45:45 +0530

    Chosing days of week using switch

---
+++ DaysChoice.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DaysChoice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the day of week: ");
+        int week = sc.nextInt();
+        switch (week) {
+            case 1 -> System.out.println("Today is Monday");
+            case 2 -> System.out.println("Today is Tuesday");
+            case 3 -> System.out.println("Today is Wednesday");
+            case 4 -> System.out.println("Today is Thursday");
+            case 5 -> System.out.println("Today is Friday");
+            case 6 -> System.out.println("Today is Saturday");
+            case 7 -> System.out.println("Today is Sunday");
+            default -> System.out.println("Invalid choice");
+        }
+
+    
+}
+}
