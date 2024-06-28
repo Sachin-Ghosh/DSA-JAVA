dab98f1c8378d8d17ccbe3aeae5a50a4da11a4af
SACHIN <ghoshsachin2301@gmail.com>
2024-06-28 13:23:38 +0530

    Calculator

---
+++ Calculator.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ans = 0;
+
+        while (true) {
+            System.out.print("Enter the operator: ");
+            char op = sc.next().trim().charAt(0);
+
+            if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
+                System.out.print("Enter the first number: ");
+                int num1 = sc.nextInt();
+                System.out.print("Enter the second number: ");
+                int num2 = sc.nextInt();
+                if (op == '+') {
+                    ans = num1 + num2;
+                } else if (op == '-') {
+                    ans = num1 - num2;
+                } else if (op == '*') {
+                    ans = num1 * num2;
+                } else if (op == '/') {
+                    if (num2 != 0) {
+                        ans = num1 / num2;
+                    }
+                } else if (op == '%') {
+                    ans = num1 % num2;
+                }}
+                else if(op=='x' || op=='X'){
+                    break;
+                }
+                else{
+                    System.out.println("Invalid operator");
+                }
+                System.out.println("The answer is: " + ans);
+            }
+
+        }
+    }
