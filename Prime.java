666689cb5daa9ba2b62a84daec4a15364dd87091
SACHIN <ghoshsachin2301@gmail.com>
2024-06-29 19:07:40 +0530

    Prime numbers using method

---
+++ Prime.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        boolean ans = isPrime(num);
+        System.out.println(ans);
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        int c = 2;
+        while (c * c <= num) {
+            if (num % c == 0) {
+                return false;
+            }
+            c++;
+        }
+        return c * c > num;
+    }
+}
