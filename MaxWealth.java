ca475d68748d912736f436ac87c511b3d01e7759
SACHIN <ghoshsachin2301@gmail.com>
2024-07-10 00:03:22 +0530

    Richest Customer Wealth

--- MaxWealth.java
+++ MaxWealth.java
@@ -3,18 +3,33 @@ public class MaxWealth {
         int[][] accounts = {{1,2,3},{3,2,1}};
         System.out.println(maximumWealth(accounts));
         }
-        public static int maximumWealth(int[][] accounts) {
-            int max = Integer.MIN_VALUE;
-            for (int i = 0; i < accounts.length; i++) {
-                int sum = 0;
-                for (int j = 0; j < accounts[i].length; j++) {
-                    sum += accounts[i][j];
-                    }
-                    if (sum > max) {
-                        max = sum;
-                        }
-                        }
-                        return max;
+    //     public static int maximumWealth(int[][] accounts) {
+    //         int max = Integer.MIN_VALUE;
+    //         for (int i = 0; i < accounts.length; i++) {
+    //             int sum = 0;
+    //             for (int j = 0; j < accounts[i].length; j++) {
+    //                 sum += accounts[i][j];
+    //                 }
+    //                 if (sum > max) {
+    //                     max = sum;
+    //                     }
+    //                     }
+    //                     return max;
+    // }
+    public static int maximumWealth(int[][] accounts) {
+        int m = Integer.MIN_VALUE;
+        
+        for (int[] customer : accounts) {
+            int n = 0;
+            for (int wealth : customer) {
+                n += wealth;
+            }
+            if (n > m) {
+                m = n;
+            }
+        }
+        
+        return m;
     }
     
 }
