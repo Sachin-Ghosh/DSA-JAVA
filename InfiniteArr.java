939cec8d34ccf6fdec925f8c3d58b2f45cef1fcd
SACHIN <ghoshsachin2301@gmail.com>
2024-07-24 15:56:57 +0530

    Find position of an element in a sorted array of infinite numbers

---
+++ InfiniteArr.java
@@ -0,0 +1,39 @@
+public class InfiniteArr {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,9,10,12,13,14,16,18,19,20,25,29,30};
+        int target = 25;
+        System.out.println(ans(arr,target));
+    }
+    public static int ans(int[] arr,int target){
+        int start = 0;
+        int end = 1;
+        while(target > arr[end]){
+            int temp = end + 1;
+            end = end + (end - start + 1) * 2;
+            start = temp;
+        }
+        return binarySearch(arr, target, start, end);
+    }
+    public static int binarySearch(int[] arr,int target,int start,int end){
+        
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(arr[mid] == target){
+                return mid;
+                }
+                else if(arr[mid] < target){
+                        start = mid + 1;
+                        }
+                    else if(arr[mid] > target){
+                        end = mid - 1;
+                        }
+                        else{
+                            return mid;
+                        }
+                        }
+                        
+                        return -1;
+    }
+    
+}
+
