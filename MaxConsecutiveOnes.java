44072b44b988bb5a9d5c5fefd3b96aa298eee465
SACHIN <ghoshsachin2301@gmail.com>
2024-07-29 23:37:59 +0530

    Max Consecutive Ones

---
+++ MaxConsecutiveOnes.java
@@ -0,0 +1,22 @@
+public class MaxConsecutiveOnes {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 0, 1, 1, 1};
+
+        System.out.println(findMaxConsecutiveOnes(nums));
+    }
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int maxCount = 0;
+        int currentCount = 0;
+
+        for (int num : nums) {
+            if (num == 1) {
+                currentCount++;
+            } else {
+                maxCount = Math.max(maxCount, currentCount);
+                currentCount = 0;
+            }
+        }
+        
+        return Math.max(maxCount, currentCount);
+    }
+}
