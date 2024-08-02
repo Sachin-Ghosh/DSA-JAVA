6d08a282d1d3db5184d99294b2976e9022c6bbe5
SACHIN <ghoshsachin2301@gmail.com>
2024-08-02 19:09:09 +0530

    Majority Element

---
+++ MajorityElement.java
@@ -0,0 +1,19 @@
+public class MajorityElement {
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 3};
+        System.out.println(majorityElement(nums));
+    }
+    public static int majorityElement(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+}
