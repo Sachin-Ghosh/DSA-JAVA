8e82b690f7bdf1e09c9bafdd178560273fcc3920
SACHIN <ghoshsachin2301@gmail.com>
2024-08-07 17:14:21 +0530

    Minimum Number of Days to Make m Bouquets

---
+++ MinDaysToMakeBouquets.java
@@ -0,0 +1,49 @@
+public class MinDaysToMakeBouquets {
+    public static void main(String[] args) {
+        int[] bloomDay = {1, 10, 3, 10, 2};
+        int m = 3, k = 1;
+        System.out.println(minDays(bloomDay, m, k));
+    }
+    public static int minDays(int[] bloomDay, int m, int k) {
+        int n = bloomDay.length;
+        if (m * k > n) {
+            return -1;
+        }
+
+        int left = Integer.MAX_VALUE;
+        int right = Integer.MIN_VALUE;
+        
+        for (int day : bloomDay) {
+            left = Math.min(left, day);
+            right = Math.max(right, day);
+        }
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (canMakeBouquets(bloomDay, m, k, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1; 
+            }
+        }
+        return left;
+    }
+
+    private static boolean canMakeBouquets(int[] bloomDay, int m, int k, int days) {
+        int bouquets = 0;
+        int flowers = 0;
+        
+        for (int day : bloomDay) {
+            if (day <= days) {
+                flowers++;
+                if (flowers == k) {
+                    bouquets++;
+                    flowers = 0;
+                }
+            } else {
+                flowers = 0;
+            }
+        }
+        return bouquets >= m;
+    }
+}
